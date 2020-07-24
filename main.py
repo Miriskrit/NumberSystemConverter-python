279175936077516d72a1c1a7015ccacfeb923fa3
Miriskrit <miha1502@mail.ru>
2020-07-24 13:14:49 +0300

    renamed base variables

--- main.py
+++ main.py
@@ -15,21 +15,20 @@ from translate import NumberSystemTranslation
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
 
-
-
-
 class Container(BoxLayout):
-    main_input = ObjectProperty()
-    from_system_input = ObjectProperty()
-    to_system_input = ObjectProperty()
-    result = ObjectProperty()
+    translator_translator_main_input = ObjectProperty()
+    translator_from_numeral_system_input = ObjectProperty()
+    translator_to_numeral_system_input = ObjectProperty()
+    translator_translator_result_label_label = ObjectProperty()
+
     calc_left_input = ObjectProperty()
-    calc_left_s = ObjectProperty()
+    calc_left_system_input = ObjectProperty()
     calc_right_input = ObjectProperty()
-    calc_right_s = ObjectProperty()
-    calc_decision = ObjectProperty()
-    calc_result = ObjectProperty()
-    calc_result_input = ObjectProperty()
+    calc_right_system_input = ObjectProperty()
+    calc_before_translator_result_label_label = ObjectProperty()
+    calc_result_label_label = ObjectProperty()
+    calc_result_system_input = ObjectProperty()
+
     last_do = 'pls'
 
     def format_text(self, text, t):
@@ -61,38 +60,38 @@ class Container(BoxLayout):
 
     def change_value(self, side, value):
         try:
-            i_left = int(self.from_system_input.text)
+            i_left = int(self.translator_from_numeral_system_input.text)
         except:
             i_left = 8
 
         try:
-            i_right = int(self.to_system_input.text)
+            i_right = int(self.translator_to_numeral_system_input.text)
         except:
             i_right = 8
 
         if side == 'left':
             if value == 'up' and i_left < 24:
-                self.from_system_input.text = str(i_left+1)
+                self.translator_from_numeral_system_input.text = str(i_left+1)
             elif value == 'down' and i_left > 2:
-                self.from_system_input.text = str(i_left-1)
+                self.translator_from_numeral_system_input.text = str(i_left-1)
         else:
             if value == 'up' and i_right < 24:
-                self.to_system_input.text = str(i_right+1)
+                self.translator_to_numeral_system_input.text = str(i_right+1)
             elif value == 'down' and i_right > 2:
-                self.to_system_input.text = str(i_right-1)
+                self.translator_to_numeral_system_input.text = str(i_right-1)
 
     #
 
     def switch(self):
-        self.from_system_input.text, self.to_system_input.text = self.to_system_input.text, self.from_system_input.text
+        self.translator_from_numeral_system_input.text, self.translator_to_numeral_system_input.text = self.translator_to_numeral_system_input.text, self.translator_from_numeral_system_input.text
 
     #
 
     def clean(self):
-        self.from_system_input.text = ''
-        self.to_system_input.text = ''
-        self.main_input.text = ''
-        self.result.text = ''
+        self.translator_from_numeral_system_input.text = ''
+        self.translator_to_numeral_system_input.text = ''
+        self.translator_main_input.text = ''
+        self.translator_result_label.text = ''
 
     #
 
@@ -103,42 +102,42 @@ class Container(BoxLayout):
                 t = int(t)
                 s = int(s)
                 if (int(t) < 2 or int(t) > 24) or (int(s) < 2 or int(s) > 24):
-                    self.result.text = '[color=ff3333]Доступные системы счиления\nот 2 до 24[/color]'
+                    self.translator_result_label.text = '[color=ff3333]Доступные системы счиления\nот 2 до 24[/color]'
                 if int(t) < 2:
-                    self.to_system_input.text = '2'
+                    self.translator_to_numeral_system_input.text = '2'
                 elif int(t) > 24:
-                    self.to_system_input.text = '24'
+                    self.translator_to_numeral_system_input.text = '24'
 
                 if int(s) < 2:
-                    self.from_system_input.text = '2'
+                    self.translator_from_numeral_system_input.text = '2'
                 elif int(s) > 24:
-                    self.from_system_input.text = '24'
+                    self.translator_from_numeral_system_input.text = '24'
                 return True
             except:
                 return False
 
         def make_mathematic():
-            user_number = self.main_input.text.upper()
-            from_system = self.from_system_input.text
-            to_system = self.to_system_input.text
+            user_number = self.translator_main_input.text.upper()
+            from_system = self.translator_from_numeral_system_input.text
+            to_system = self.translator_to_numeral_system_input.text
             if is_correct_input(to_system, from_system):
                 try:
-                    text = NumberSystemTranslation().make_translation(user_number, from_system, to_system)
+                    text = NumberSystemTranslation().make_translation(
+                        user_number, from_system, to_system)
                     if text.isdigit():
                         text = self.format_text(
                             text, to_system) + '[sub]({})[/sub]'.format(to_system)
-                    self.result.text = text
+                    self.translator_result_label.text = text
                 except:
-                    self.result.text = '[color=ff3333]Ошибка[/color]'
+                    self.translator_result_label.text = '[color=ff3333]Ошибка[/color]'
             else:
-                self.result.text = '[color=ff3333]Вы не ввели число[/color]'
+                self.translator_result_label.text = '[color=ff3333]Вы не ввели число[/color]'
         make_mathematic()
-
 
     def to_count(self, mode):
 
         def calc_is_correct_input():
-            for txtinput in (self.calc_left_s, self.calc_right_s, self.calc_result_input):
+            for txtinput in (self.calc_left_system_input, self.calc_right_system_input, self.calc_result_system_input):
                 try:
                     i = int(txtinput.text)
                     if i < 2:
@@ -154,11 +153,12 @@ class Container(BoxLayout):
 
         def convert_both_to_ten():
             c = NumberSystemTranslation()
-            return c.make_translation(self.calc_left_input.text.upper(), int(self.calc_left_s.text), 10), \
-                    c.make_translation(self.calc_right_input.text.upper(), int(self.calc_right_s.text), 10)
+            return c.make_translation(self.calc_left_input.text.upper(), int(self.calc_left_system_input.text), 10), \
+                c.make_translation(self.calc_right_input.text.upper(), int(
+                    self.calc_right_system_input.text), 10)
 
         def make_mathematic():
-            
+
             def mathematical_operation(name, isfloat, first_num, second_num):
                 if name == 'min':
                     res = float(first_num)-float(second_num)
@@ -173,56 +173,56 @@ class Container(BoxLayout):
                     res = round(float(first_num)/float(second_num), 5)
                     if res == int(res):
                         res = int(res)
-                        
+
                 self.last_do = name
-                
-                
+
                 if not isfloat:
                     return int(res)
                 else:
                     return res
-            
+
             number_is_null = False
-            final_syst = self.calc_result_input.text
+            final_syst = self.calc_result_system_input.text
             first_num, second_num = convert_both_to_ten()
             c = NumberSystemTranslation()
             try:
                 first_num = c.make_translation(self.calc_left_input.text.upper(),
-                                   int(self.calc_left_s.text), 10)
+                                               int(self.calc_left_system_input.text), 10)
                 second_num = c.make_translation(self.calc_right_input.text.upper(),
-                                    int(self.calc_right_s.text), 10)
-                
+                                                int(self.calc_right_system_input.text), 10)
+
                 if '.' in first_num or '.' in second_num:
                     isfloat = True
                 else:
                     isfloat = False
-                
-                ten_system_res = mathematical_operation(mode, isfloat, first_num, second_num)
-                if ten_system_res == 0:
-                        number_is_null = True
 
+                ten_system_res = mathematical_operation(
+                    mode, isfloat, first_num, second_num)
+                if ten_system_res == 0:
+                    number_is_null = True
 
                 modes = {'min': '-', 'pls': '+', 'mlt': '*', 'del': '/'}
-                self.calc_decision.text = f'{first_num}[sub](10)[/sub] {modes.get(mode)} {second_num}[sub](10)[/sub] = {ten_system_res}[sub](10)[/sub]'
+                self.calc_before_result_label.text = f'{first_num}[sub](10)[/sub] {modes.get(mode)} {second_num}[sub](10)[/sub] = {ten_system_res}[sub](10)[/sub]'
 
                 if not number_is_null:
-                    final_res = str(c.make_translation(str(ten_system_res), 10, int(final_syst)))
+                    final_res = str(c.make_translation(
+                        str(ten_system_res), 10, int(final_syst)))
                     if isfloat:
                         final_res = float(final_res)
                     else:
                         final_res = int(final_res)
-                        
+
                     if str(final_res).find('[') == -1:
-                        self.calc_result.text = self.format_text(
+                        self.calc_result_label.text = self.format_text(
                             str(final_res), int(final_syst)) + '[sub]({})[/sub]'.format(final_syst)
                     else:
                         # если обнаружена ошибка
-                        self.calc_result.text = final_res
+                        self.calc_result_label.text = final_res
                 else:
-                    self.calc_result.text = '0'
+                    self.calc_result_label.text = '0'
             except Exception as exc:
                 print(exc)
-                self.calc_result.text = '[color=ff3333]Ошибка[/color]'
+                self.calc_result_label.text = '[color=ff3333]Ошибка[/color]'
 
         calc_is_correct_input()
         make_mathematic()
@@ -236,29 +236,27 @@ class Container(BoxLayout):
 
     def calc_change_value(self, value):
         try:
-            i = int(self.calc_result_input.text)
+            i = int(self.calc_result_system_input.text)
         except:
             i = 8
 
         if i > 1 and value > 0:
-            self.calc_result_input.text = str(i + 1)
+            self.calc_result_system_input.text = str(i + 1)
             self.calc_repeat()
         elif i < 25 and value < 0:
-            self.calc_result_input.text = str(i - 1)
+            self.calc_result_system_input.text = str(i - 1)
             self.calc_repeat()
 
     #
 
     def calc_clean(self):
-        self.calc_result_input.text = ''
+        self.calc_result_system_input.text = ''
         self.calc_left_input.text = ''
         self.calc_right_input.text = ''
-        self.calc_left_s.text = ''
-        self.calc_right_s.text = ''
-        self.calc_decision.text = ''
-        self.calc_result.text = ''
-
-
+        self.calc_left_system_input.text = ''
+        self.calc_right_system_input.text = ''
+        self.calc_before_result_label.text = ''
+        self.calc_result_label.text = ''
 
 
 class MyApp(MDApp):
